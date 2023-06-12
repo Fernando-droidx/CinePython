cd6b1d9f8c7503e82276bb97636507bc907886ae
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2023-06-12 13:24:13 -0600

    Agregando leeer archivo en la funcion

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ print(pelicula.menu())
 nombre = input("Ingresa tu nombre: ")
 pelicula = input("Ingresa la pelicula que desees ver: ")
 asientos = int(input("Ingresa el numero de asientos"))
-
+print("----------------------------------------------------------")
 print(precio.precio(asientos))
+print("----------------------------------------------------------")
 print(ticket.ticekt(nombre,pelicula,asientos))
+print("----------------------------------------------------------")
+
