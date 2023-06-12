12335e4490b0f3c11e38f3ecf6fecd5b4149c78e
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2023-06-12 13:26:28 -0600

    jejeje

--- ticket.py
+++ ticket.py
@@ -1,6 +1,6 @@
 def ticekt(nombre,asientos, pelicula):
     archivo = open('ticket.txt', 'w')
-    nombre = f"A nombre de {nombre} para la peli de {asientos}, Con asientos de: {pelicula}"
+    nombre = f"A nombre de {nombre} para la peli de {asientos}, Con asientos de: {pelicula} "
     archivo.write(nombre)
     archivo.close()
     print("Archivo en txt")
